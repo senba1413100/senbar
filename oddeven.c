684f19216b68fa6c5ce187fd9ebb90783867a9ec
senba1413100 <senbagavalliksr97@gmail.com>
2016-12-22 14:29:25 +0530

    Add files via upload

--- oddeven.c
+++ oddeven.c
@@ -6,10 +6,9 @@ int num;
 printf("enter the number:");
 scanf("%d",&num);
 if(num%2==0)
-{
-printf("the number is even")
+printf("the number is even");
 else
 printf("the number is odd");
-}
+
 getch();
 }
